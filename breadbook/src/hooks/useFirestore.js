6542dd1b7ae1df2067cf204cc6269d19fde0079c
Karesz <torok.karoly.krisztian@gmail.com>
2020-08-21 16:00:17 +0200

    Image uploading finished

---
+++ breadbook/src/hooks/useFirestore.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { projectFirestore } from '../firebase/config';
+
+
+const useFirestore = (collection) => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = projectFirestore.collection(collection)
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snapshot) => {
+        let documents = [];
+        snapshot.forEach((doc) => {
+          documents.push({...doc.data(), id: doc.id});
+        });
+        setDocs(documents);
+      });
+    return () => unsubscribe();
+  }, [collection]);
+
+  return docs;
+};
+
+export default useFirestore;
